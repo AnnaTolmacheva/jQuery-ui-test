065f4efb1ced57cb15e0b60b49bcb6d1b1def72a
AnnaTolmacheva <ann3101@mail.ru>
2021-07-15 11:33:52 +0300

    first commit

---
+++ t1.js
@@ -0,0 +1,7 @@
+$( function() {
+    $( "#accordion" ).accordion();
+    $( "#accordion" ).on( "accordionactivate", ( event, ui ) => {
+        $( ui.newPanel ).css( "background-color", "yellow" );
+        $( ui.oldPanel ).css( "background-color", "blue" );
+    } );
+  } );
